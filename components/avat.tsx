29432b777c54225879381bdc880dfa10e652f98b
LucianoVelasquez <125805986+LucianoVelasquez@users.noreply.github.com>
2024-07-03 10:35:05 -0300

    fixed

--- components/avat.tsx
+++ components/avat.tsx
@@ -4,14 +4,14 @@ import { Linkedin,Github } from 'lucide-react';
 export default function Avat() {
   return (
     <div className="flex justify-center items-center gap-x-5 absolute top-5 left-8">
-    <a href="https://www.linkedin.com/in/lv-dev/"><Linkedin size={32}></Linkedin></a>
-    <a href="https://veldev.vercel.app/">
+    <a href="https://www.linkedin.com/in/lv-dev/" target="_blank"><Linkedin size={32}></Linkedin></a>
+    <a href="https://veldev.vercel.app/" target="_blank">
         <Avatar className="w-10 h-10">
         <AvatarImage  src="https://raw.githubusercontent.com/LucianoVelasquez/portfolio/main/src/components/Hero/avatar3.png" />
         <AvatarFallback>CN</AvatarFallback>
         </Avatar>
     </a>
-    <a  href="https://github.com/LucianoVelasquez"><Github size={32}></Github></a>
+    <a  href="https://github.com/LucianoVelasquez" target="_blank"><Github size={32}></Github></a>
     </div>   
     
   );
